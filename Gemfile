a11d03ce614cd69cb3d71ef9517b04c50ca5978b
= <herniaan@gmail.com>
2016-05-03 09:06:33 +0900

    make calc page

---
+++ Gemfile
@@ -0,0 +1,3 @@
+# If you do not have OpenSSL installed, change
+# the following line to use 'http://'
+source 'https://rubygems.org'
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,29 @@
-console.log('This would be the main JS file.');
+function calc() {
+  var input = $('#input').val();
+  var data = input.split('\n');
+  
+  money = 0
+  money_taka = 0
+  money_miho = 0
+  
+  for(i in data) {
+    var l = data[i];
+
+    switch( true ) {
+    case /\d\d\/\d\d\(\S\)/.test(l):
+      money = 0;
+      break;
+    case /-[\d,]+/.test(l):
+      money = Number(l.match(/-[\d,]+/)[0].replace(/,/, ''));
+      break;
+    case /たかひろ/.test(l):
+      money_taka += money;
+      break;
+    case /みほ/.test(l):
+      money_miho += money;
+      break;
+    }    
+  }
+
+  $('#output').val("たかひろ: " + money_taka + "\n" + "みほ: " + money_miho);
+}
